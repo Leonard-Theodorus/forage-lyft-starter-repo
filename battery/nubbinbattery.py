0f58489d4300c2b54caccfecaab5514c6bd67f85
Leonard Theodorus <aoesotfs@gmail.com>
2023-12-16 19:04:46 +0700

    feat: fixed year calc on battery

--- battery/nubbinbattery.py
+++ battery/nubbinbattery.py
@@ -1,9 +1,11 @@
 from battery import Battery
+from util import add_years_to_date
 
 class NubbinBattery(Battery):
-    def __init__(self, currentYear, lastServiceYear):
-        self.currentYear = currentYear
-        self.lastServiceYear = lastServiceYear
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
         #PerYear = 4
     def needsService(self) -> bool:
-        return (self.currentYear - self.lastServiceYear) % 4 == 0
+        needs_to_be_serviced_by = add_years_to_date(self.last_service_date, 4)
+        return needs_to_be_serviced_by < self.current_date
--- battery/spindlerbattery.py
+++ battery/spindlerbattery.py
@@ -1,9 +1,11 @@
 from battery import Battery
+from util import add_years_to_date
 
 class SpindlerBattery(Battery):
-    def __init__(self, currentYear, lastServiceYear):
-        self.currentYear = currentYear
-        self.lastServiceYear = lastServiceYear
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_year = last_service_date
         #PerYear = 2
     def needsService(self) -> bool:
-        return (self.currentYear - self.lastServiceYear) % 2 == 0
+        needs_to_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        return needs_to_be_serviced_by < self.current_date
